92d3315368219d46b21f8072769022e21d77904f
HeraldiePro <heraldiepro@gmail.com>
2021-08-29 17:26:53 +0200

    Ajout des tests unitaires associées

--- cypress/integration/etape2_spec.js
+++ cypress/integration/etape2_spec.js
@@ -1,53 +1,64 @@
 /* eslint-disable no-undef */
-describe('Etape 1', function() {
+describe('Etapes 2:', function() {
     beforeEach(function() {
         cy.visit('/enigmes');
+        
+        cy.get("[data-testid=input]")
+        .type('24,3')
+            
+        cy.get('[data-testid=Submit]')
+        .click()
+
+        cy.get('[data-testid=NextStep]')
+        .should('be.visible')
+        .click()
     });
-    it('Test title Etape1',() => {
+
+    it('Test title Etape2',() => {
         cy.get('[data-testid=Title]')
         .should('be.visible')
-        .should('have.text', 'Employé polyvalent en restauration rapide')
+        .should('have.text', 'Animation enfant')
     })
 
-    it('Test p1 Etape1',() => {
+    it('Test p1 Etape2',() => {
         cy.get('[data-testid=p1]')
         .should('be.visible')
-        .should('have.text', 'Comme beaucoup de jeunes actif je commençai par travailler en restauration rapide, je dois bien\nadmettre que ce domaine professionnel bien que très éloigné de mes valeurs fut au final une\nexpérience intéressante car elle m’a permis de développer mon contact clientèle en travaillant en\ncaisse.')
+        .should('have.text', `Suite à une reconversion professionnel je décidâmes ensuite de m’orienter vers l’animation jeune\npublic, pour ce faire j’ai fait le parcours classique du BAFA et direct après son acquisition j’ai pu\ncommencer à travailler pour une association pour les temps de périscolaire. J’avais d’ailleurs déjà à\ncœur de masteriser et d’initier mon public à différents jeux de société et/ou de stratégie.`)
     })
 
-    it('Test h2 Etape1',() => {
+    it('Test h2 Etape2',() => {
         cy.get('[data-testid=h2]')
         .should('be.visible')
         .should('have.text', "L'énigme est donc la suivante")
     })
 
-    it('Test enoncer Etape1',() => {
+    it('Test enoncer Etape2',() => {
         cy.get('[data-testid=enoncer]')
         .should('be.visible')
-        .should('have.text', 'Arthur et mathilde souhaite commander 2 menus mega Sunday ainsi qu’un cheeseburger chacun il\ncommande ensemble et dispose d’une carte étudiant. Qu’elle somme devront -ils régler ?')
+        .should('have.text', 'Décrifrer le rébu suivant : Ajouter liste des images ')
     })
 
-    it('Test ContentCard Etape1',() => {
+    it('Test ContentCard Etape2',() => {
         cy.get('[data-testid=ContentCard]')
         .should('be.visible')
     })
 
-    it('Test inputEtape1 Etape1',() => {
+    it('Test inputEtape1 Etape2',() => {
         cy.get('[data-testid=input]')
         .should('be.visible')
     })
     
-    it('Test SubmitEtape1 Etape1',() => {
+    it('Test SubmitEtape1 Etape2',() => {
         cy.get('[data-testid=Submit]')
         .should('be.visible')
     })
 
-    it('Test NextStep Etape1',() => {
+    it('Test NextStep Etape2',() => {
         cy.get('[data-testid=NextStep]')
         .should('be.not.visible')
     })
 
-    it('False answer Etape1',() => {
+    it('False answer Etape2',() => {
         cy
         .get("[data-testid=input]")
         .invoke('val', 'error')
@@ -71,13 +82,13 @@ describe('Etape 1', function() {
         cy
         .get("[data-testid=textError]")
         .should('be.visible')
-        .should('have.text','Indice : pour calculer facilement une réduction de 10% diviser par 10 le résultat initial ou bien d’aller\nsur le site du service publique et de faire une simulation de calcul 😊.')
+        .should('have.text','Indice : le personnage dessinée est russe et pour le plateau il s’agit célèbre jeux de stratégie coréen. 😊.')
     })
 
-    it('Good answer Etape1',() => {
+    it('Good answer Etape2',() => {
         cy
         .get("[data-testid=input]")
-        .type('24,3')
+        .type('pédagogie')
         
         cy.get('[data-testid=Submit]')
         .click()
@@ -92,6 +103,6 @@ describe('Etape 1', function() {
 
         cy.get('[data-testid=Title]')
         .should('be.visible')
-        .should('have.text','Animation enfant')
+        .should('have.text','Animation village vacances')
     })
-});
+})
---
+++ cypress/integration/etape3_spec.js
@@ -0,0 +1,123 @@
+/* eslint-disable no-undef */
+describe('Etapes 3:', function() {
+    beforeEach(function() {
+        cy.visit('/enigmes');
+        
+        cy.get("[data-testid=input]")
+        .type('24,3')
+            
+        cy.get('[data-testid=Submit]')
+        .click()
+
+        cy.get('[data-testid=NextStep]')
+        .should('be.visible')
+        .click()
+
+        cy
+        .get("[data-testid=input]")
+        .type('pédagogie')
+        
+        cy.get('[data-testid=Submit]')
+        .click()
+        
+        cy
+        .get("[data-testid=succes]")
+        .should('be.visible')
+
+        cy.get('[data-testid=NextStep]')
+        .should('be.visible')
+        .click()
+    });
+
+    it('Test title',() => {
+        cy.get('[data-testid=Title]')
+        .should('be.visible')
+        .should('have.text', 'Animation village vacances')
+    })
+
+    it('Test p1',() => {
+        cy.get('[data-testid=p1]')
+        .should('be.visible')
+        .should('have.text', `De fil en aiguille j’ai souhaité expérimenter d’animer des public dans des structures différentes avec\ndes public plus varié, j’ai donc eu la chance de travailler en saison pour un village vacance 4 étoile\nsitué sur la côte d’azur du nom très rigolo de TourisTra.Au cours de cette expérience j’ai eu l’occasion de mettre à profit mes compétences en théâtre en\nmontant sur scène (et parfois même de faire du travestie hihi) d’apprendre des rudiments de\nprestidigitation, d’improviser un jeu de piste avec pour élément principal une bobine de laine et aussi\nd’apprendre des chorégraphie (pour mon plus grand plaisir) pour des animations avec les enfants qui\navait lieu 5 jours sur 7 (le principe de mini si vous connaissez).`)
+    })
+
+    it('Test h2',() => {
+        cy.get('[data-testid=h2]')
+        .should('be.visible')
+        .should('have.text', "L'énigme est donc la suivante")
+    })
+
+    it('Test enoncer',() => {
+        cy.get('[data-testid=enoncer]')
+        .should('be.visible')
+        .should('have.text', ' Veuillez trouver parmi la liste ci-dessous et en vous aidant du texte ci-dessus l’intrus dans cette liste d’activités ')
+    })
+
+    it('Test ContentCard',() => {
+        cy.get('[data-testid=ContentCard]')
+        .should('be.visible')
+    })
+
+    it('Test inputEtape1',() => {
+        cy.get('[data-testid=input]')
+        .should('be.visible')
+    })
+    
+    it('Test SubmitEtape1',() => {
+        cy.get('[data-testid=Submit]')
+        .should('be.visible')
+    })
+
+    it('Test NextStep',() => {
+        cy.get('[data-testid=NextStep]')
+        .should('be.not.visible')
+    })
+
+    it('False answer',() => {
+        cy
+        .get("[data-testid=input]")
+        .invoke('val', 'error')
+        
+        cy.get('[data-testid=Submit]')
+        .click()
+        
+
+        cy.get('[data-testid=NextStep]')
+        .should('be.not.visible')
+
+        cy.get('[data-testid=Submit]')
+        .click()
+        
+        cy.get('[data-testid=Submit]')
+        .click()
+        
+        cy.get('[data-testid=Submit]')
+        .click()
+        
+        cy
+        .get("[data-testid=textError]")
+        .should('be.visible')
+        .should('have.text','Indice :  😊.')
+    })
+
+    it('Good answer',() => {
+        cy
+        .get("[data-testid=input]")
+        .type('sport')
+        
+        cy.get('[data-testid=Submit]')
+        .click()
+        
+        cy
+        .get("[data-testid=succes]")
+        .should('be.visible')
+
+        cy.get('[data-testid=NextStep]')
+        .should('be.visible')
+        .click()
+
+        cy.get('[data-testid=Title]')
+        .should('be.visible')
+        .should('have.text','Formation ATL et stage en escape game')
+    })
+})
