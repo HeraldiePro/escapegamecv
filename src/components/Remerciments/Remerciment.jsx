b9b5507e74829966a9ab0ebf9668eb564d81b01b
HeraldiePro <heraldiepro@gmail.com>
2021-10-01 12:39:49 +0200

    feat: correctif téléchargement du cv

--- src/components/Remerciments/Remerciment.jsx
+++ src/components/Remerciments/Remerciment.jsx
@@ -6,7 +6,7 @@ import {
 import { makeStyles } from '@material-ui/core/styles'
 import ButtonCustom from '../../theme/Button'
 import { colors } from '../../theme/variables';
-import cv from '../../assets/pdf/_Cv antoine doubinine animation escape game.pdf'
+import cv from '../../assets/pdf/cv_antoine.pdf'
 import GetAppIcon from '@material-ui/icons/GetApp';
 
 const useStyles = makeStyles((theme) => ({
